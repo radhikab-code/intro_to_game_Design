d0afb8152713207ac1801ec658effae69869ebbc
radhikab-code <radhika_b@nid.edu>
2025-10-10 00:29:54 +0530

    class work

---
+++ assignment_4.js
@@ -0,0 +1,26 @@
+let size = 70; genImage=[]; noImage=3;
+
+function preload() {
+  for(let i=0; i<noImage; i++) {
+    //let name = "images/i_"+i+".png";
+    let name = "images/line_img"+i+".png";
+    genImage[i]= loadImage(name);
+  }
+}
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  frameRate(1);
+  }
+
+function draw() {
+  background("light brown");
+  //create grid of squares
+  for(let i=0; i<width; i=i+size){
+    for (let j=0; j<height ; j=j+size) {
+     
+      //select 1 of 4 choice
+      let choice =  floor(random(0,noImage));
+      image(genImage[choice],i,j,size,size);
+       }
+  }
+}
--- image_grid_exercise.js
+++ image_grid_exercise.js
@@ -1,11 +1,15 @@
-let size = 50; genImage=[]; noImage=0
+let size = 50; genImage=[]; noImage=4;
 //g0,g1,g2,g3;
 
 function preload() {
-  g0 = loadImage("images/img0.png");
+  for(let i=0; i<noImage; i++) {
+    let name = "images/img"+i+".png";
+    genImage[i]= loadImage(name);
+  }
+ /* g0 = loadImage("images/img0.png");
   g1 = loadImage("images/img1.png");
   g2 = loadImage("images/img2.png");
-  g3 = loadImage("images/img3.png");
+  g3 = loadImage("images/img3.png");*/
 }
 function setup() {
   createCanvas(innerWidth, innerHeight);
@@ -20,16 +24,17 @@ function draw() {
     for (let j=0; j<height ; j=j+size) {
      
       //select 1 of 4 choice
-      let choice =  floor(random(0,4));
-      if(choice==0) {
-        image(g0,i,j,size,size);
-      } else if (choice==1){
-        image(g1,i,j,size,size);
-      } else if(choice=2) {
-        image(g2,i,j,size,size);
-      } else{
-        image(g3,i,j,size,size);
-      }
+      let choice =  floor(random(0,noImage));
+      image(genImage[choice],i,j,size,size);
+      // if(choice==0) {
+      //   image(g0,i,j,size,size);
+      // } else if (choice==1){
+      //   image(g1,i,j,size,size);
+      // } else if(choice=2) {
+      //   image(g2,i,j,size,size);
+      // } else{
+      //   image(g3,i,j,size,size);
+      // }
     }
   }
 }
